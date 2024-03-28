2efe5af1840ca89ddd3f06ab671431d8d4167033
addinaamiruddin <78295393+addinaamiruddin@users.noreply.github.com>
2024-03-29 00:59:49 +0800

    adding, read and display csv

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
@@ -10,7 +9,7 @@ function App() {
   return (
     <>
       <Router>
-        <NavBar />
+        {/* <NavBar /> */}
         <Routes>
           <Route path='/' exact element={ <Home />}></Route>
         </Routes>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,52 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import CardItem from "./CardItem";
 import "./Cards.css";
+import { useEffect } from "react";
+import Papa from "papaparse";
+import Data from "./testing_data.csv";
+// import { Link } from "react-router-dom";
+import PokemonFetcher from "./PokemonFetcher";
 
 function Cards() {
+  const [data, setData] = useState([]);
+  const [pokemonData, setPokemonData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(Data);
+      const reader = response.body.getReader();
+      const result = await reader.read();
+      const decoder = new TextDecoder("utf-8");
+      const csvData = decoder.decode(result.value);
+      const parseData = Papa.parse(csvData, {
+        header: true,
+        skipEmptyLines: true,
+      }).data;
+      setData(parseData);
+    };
+    fetchData();
+  }, []);
+
+  const handlePokemonData = (pokemonData) => {
+    setPokemonData(pokemonData);
+  };
+
   return (
     <div className="cards">
       <h1>Check out these epic destinations</h1>
       <div className="cards__container">
         <div className="cards_wrapper">
-        <ul className='cards__items'>
-            <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
-            />
+          <ul className="cards__items">
+            {data.length
+              ? data.map((row, index) => (
+                  <CardItem
+                    key={index}
+                    src={row.src}
+                    text={row.text}
+                    label={row.label}
+                    path={row.path}
+                  />
+                ))
+              : null}
+            {pokemonData && (
+              <CardItem
+                src={pokemonData.sprites.front_default}
+                text={`Name: ${pokemonData.name}`}
+                label={`Type: ${pokemonData.types[0].type.name}`}
+                path={`/pokemon/${pokemonData.id}`}
+              />
+            )}
           </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
-            />
-            <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
-            />
-            <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
-            />
+          <ul>
+            <PokemonFetcher onPokemonData={handlePokemonData} />
           </ul>
         </div>
       </div>
