c8c455f2d814db488ddef4718fbe91e6fde99f75
addinaamiruddin <78295393+addinaamiruddin@users.noreply.github.com>
2024-03-29 01:39:17 +0800

    installed puppeteer

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-// import { Button } from './Button';
+import { Button } from './Button';
 import './HeroSection.css';
 
 function HeroSection() {
@@ -8,12 +8,12 @@ function HeroSection() {
     <div className='hero-container'>
       <video src="/public/videos/video-2.mp4" autoPlay loop muted />
       <div className='hero-btns'>
-        {/* <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
+        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
             GET STARTED
         </Button>
         <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
             WATCH TRAILER <i className='far fa-play-circle' />
-        </Button> */}
+        </Button>
       </div>
     </div>
   )
